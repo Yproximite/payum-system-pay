deca006db6cdbc5eb275a0f451b923019937643b
Hugo Alliaume <kocal@live.fr>
2019-01-28 15:37:30 +0100

    chore: configure PhpUnit and create a test

---
+++ Tests/AcmeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yproximite\Payum\SPPLus\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AcmeTest extends TestCase
+{
+    public function testAcme()
+    {
+        $this->assertTrue(true);
+    }
+}
